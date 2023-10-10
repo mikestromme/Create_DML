2a5147021cc52c792cae8863190dc57944290809
Mike Stromme <mstromme@suburbanenterprises.com>
2023-10-10 11:11:46 -0500

    x

---
+++ CRUD Files/delete_statements_test.sql
@@ -0,0 +1,1 @@
+DELETE FROM Forefront.dbo.JC_JOB_MASTER_MC WHERE Company_Code = 'SUB' AND Job_Number = '12345678'
---
+++ CRUD Files/select_statements_test.sql
@@ -0,0 +1,1 @@
+SELECT Master_Job FROM Forefront.dbo.JC_JOB_MASTER_MC WHERE Company_Code = 'SUB' AND Job_Number = '12345678'
